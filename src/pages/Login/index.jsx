5d97fb8c57ec4870763aed1c563c66e6e0e028ed
Ardhana Wahyu <mardhanawn@gmail.com>
2024-10-06 01:01:59 +0700

    fix: navigate username after validation

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,13 +1,19 @@
 import { Button, Form, Input } from 'antd'
+import { useNavigate } from 'react-router-dom'
 
 function Login() {
-  const onSubmit = (value) => {
-    localStorage.setItem('username', value.username)
+  const navigate = useNavigate()
+
+  const onSubmit = ({ username }) => {
+    if (username.length > 3 && username.length < 50) {
+      localStorage.setItem('username', username)
+      navigate('/dashboard')
+    }
   }
 
   return (
-    <div className="content-center h-[100vh] w-[100vw] inline-grid items-center justify-center">
-      <div className="text-3xl">Log In to Food Facts</div>
+    <div className="content-center h-screen w-screen inline-grid items-center justify-center">
+      <div className="text-3xl underline font-semibold">Log In to Food Facts</div>
       <div className="mt-5 max-w-[300px]">
         <Form
           wrapperCol={{
