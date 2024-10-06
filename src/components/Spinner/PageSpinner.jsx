37c9a4f74907a74cef0e2810195bb1dfb1ca7726
Ardhana Wahyu <mardhanawn@gmail.com>
2024-10-06 22:24:30 +0700

    feat: add components PageSpinner

---
+++ src/components/Spinner/PageSpinner.jsx
@@ -0,0 +1,11 @@
+import Spinner from '.'
+
+function PageSpinner() {
+  return (
+    <div className="h-screen w-screen flex items-center justify-center">
+      <Spinner />
+    </div>
+  )
+}
+
+export default PageSpinner
