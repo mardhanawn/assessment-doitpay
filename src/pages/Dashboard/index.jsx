47291ad92fe24e08ad3f4c84cd8f894f5a36a4d7
Ardhana Wahyu <mardhanawn@gmail.com>
2024-10-06 22:27:58 +0700

    add staleTime, cacheTIme, handle onError openNotification list
    change Spinner to pageSpinner
    
    update component usage BaseCard to ProductCard

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,25 +1,35 @@
+import { notification } from 'antd'
 import { useQuery } from 'react-query'
-import BaseCard from '../../components/Card'
-import Spinner from '../../components/Spinner'
 import ButtonLogout from '../../components/Button/ButtonLogout'
+import ProductCard from '../../components/Card/ProductCard'
+import PageSpinner from '../../components/Spinner/PageSpinner'
 import { lists_food } from '../../services/api/foodfacts'
+import openNotification from '../../utils/notification'
 
 function Dashboard() {
   const { isLoading, data } = useQuery('lists_food', lists_food, {
-    onSuccess: (data) => {
-      console.log('data dashboard return ON SUCCESS', data)
+    staleTime: Infinity,
+    cacheTime: Infinity,
+    onError: () => {
+      openNotification(api.open, {
+        message: `Oopss! Something went wrong`,
+        description: 'Please try again later',
+      })
     },
   })
 
+  const [api, contextHolder] = notification.useNotification()
+
   return (
     <div className="p-5">
+      {contextHolder}
       <div className="flex items-center justify-between">
         <div className="text-3xl underline font-semibold">Dashboard</div>
         <ButtonLogout />
       </div>
       <div className="my-5 mb-5">
         <div>Username: {localStorage.getItem('username')}</div>
-        {isLoading && <Spinner />}
+        {isLoading && <PageSpinner />}
         {
           <div>
             <div className="text-base">Product Lists</div>
@@ -31,7 +41,8 @@ function Dashboard() {
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4 my-5">
           {data?.products.map((product) => {
             return (
-              <BaseCard
+              <ProductCard
+                id={product.id}
                 key={product.id}
                 product_name={product.product_name}
                 image_url={product.image_url}
